d6dec48a3a572e1fc45c9702c1d1df63bed5a52d
Vinit Mahajan <vinitmahajan1@gmail.com>
2019-12-04 08:45:35 +0530

    delete functionality added

--- App.js
+++ App.js
@@ -11,10 +11,17 @@ export default function App() {
   const addGoalHandler = goalTitle => {
     setCourseGoal(currentGoals => [...currentGoals, {key:Math.random().toString(), value:goalTitle}]);
   }
+
+  const removeGoalHandler = goalId => {
+    setCourseGoal((currentGoals)=>{
+        return currentGoals.filter((currentGoal)=> currentGoal.key !== goalId )
+    })
+  }
+
   return (
     <View style={styles.screen}>
       <GoalInput  onAddGoal = {addGoalHandler} />
-      <FlatList data={courseGoal} renderItem = {itemData => <GoalItem value={itemData.item.value}/>} />
+      <FlatList data={courseGoal} renderItem = {itemData => <GoalItem id={itemData.item.key} onDelete={removeGoalHandler} value={itemData.item.value}/>} />
     </View>    
   );
 }
--- components/GoalItem.js
+++ components/GoalItem.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
 
 const GoalItem = props => {
     return (
-        <View style={styles.listitem}>
-            <Text>{props.value}</Text>
-        </View>
+        <TouchableOpacity activeOpacity={0.8} onPress={props.onDelete.bind(this, props.id)}>
+            <View style={styles.listitem}>
+                <Text>{props.value}</Text>
+            </View>
+        </TouchableOpacity>
+        
     )
 }
 
